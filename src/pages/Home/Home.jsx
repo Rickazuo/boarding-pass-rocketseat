f7981dc3a8feed794b91a563b2722184b6b20b9b
Rickazuo <rick3209@gmail.com>
2023-02-09 17:57:20 -0300

    add home page and styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,19 @@
 import { useEffect } from "react";
 import * as api from "../../api/flights";
+import InputPassInfo from "../../components/InputPassInfo";
 
 const Home = () => {
-    useEffect(() => {
-        const getFlights = async () => {
-            debugger;
-            const response = await api.getFlights();
-            debugger;
-        };
-        getFlights();
-    }, []);
-    return <div>oooi</div>;
+  useEffect(() => {
+    const getFlights = async () => {
+      const response = await api.getFlights();
+    };
+    getFlights();
+  }, []);
+  return (
+    <div className="teste">
+      <InputPassInfo />
+    </div>
+  );
 };
 
 export default Home;

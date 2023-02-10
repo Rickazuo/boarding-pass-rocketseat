88d79cfcafe72226ad0d3d3f6e1c009d7f97e5a2
Rickazuo <rick3209@gmail.com>
2023-02-10 10:33:57 -0300

    adjust styles of PlanningPassInfo

--- src/components/PlanningPassInfo/boarding/boarding.jsx
+++ src/components/PlanningPassInfo/boarding/boarding.jsx
@@ -2,36 +2,36 @@ import styles from "./styles.module.css";
 import qrcode from "../../../assets/qrcode.svg";
 import { getRandomInt } from "../../../utils";
 
-const Boarding = ({ time }) => {
-    return (
-        <div className={styles.boardingTag}>
-            <div className={styles.boardingContainer}>
-                <div className={styles.boardingInfo}>
-                    <div className={styles.boardingTime}>
-                        <div className={styles.spacedTitle}>Embarque</div>
-                        <div>{time}</div>
-                    </div>
-                    <div className={styles.boardingTerminal}>
-                        <div>Terminal</div>
-                        <strong>{getRandomInt(6)}</strong>
-                    </div>
-                    <div className={styles.boardingGate}>
-                        <div>Portão</div>
-                        <strong>{getRandomInt(30)}</strong>
-                    </div>
-                </div>
-                <div className={styles.boardingQrcode}>
-                    <div>
-                        <img src={qrcode} alt="qrcode image" />
-                    </div>
-                    <div>Grupo de embarque: {getRandomInt(3)}</div>
-                </div>
-            </div>
-            <div className={styles.hourAtention}>
-                <strong>Atenção:</strong> o portão fecha {time}
-            </div>
+const Boarding = ({ time, closeGateTime }) => {
+  return (
+    <div className={styles.boardingTag}>
+      <div className={styles.boardingContainer}>
+        <div className={styles.boardingInfo}>
+          <div className={styles.boardingTime}>
+            <div className={styles.spacedTitle}>Embarque</div>
+            <div>{time}</div>
+          </div>
+          <div className={styles.boardingTerminal}>
+            <div>Terminal</div>
+            <strong>{getRandomInt(6)}</strong>
+          </div>
+          <div className={styles.boardingGate}>
+            <div>Portão</div>
+            <strong>{getRandomInt(30)}</strong>
+          </div>
         </div>
-    );
+        <div className={styles.boardingQrcode}>
+          <div>
+            <img src={qrcode} alt="qrcode image" />
+          </div>
+          <div>Grupo de embarque: {getRandomInt(3)}</div>
+        </div>
+      </div>
+      <div className={styles.hourAtention}>
+        <strong>Atenção:</strong> o portão fecha {closeGateTime}
+      </div>
+    </div>
+  );
 };
 
 export default Boarding;
--- src/components/PlanningPassInfo/flight/flight.jsx
+++ src/components/PlanningPassInfo/flight/flight.jsx
@@ -3,39 +3,43 @@ import plane from "../../../assets/plane.svg";
 import { getRandomInt } from "../../../utils";
 
 const Flight = ({ date, departure, destination, time }) => {
-    return (
-        <div className={styles.flight}>
-            <div className={styles.headerFlight}>
-                <div className={styles.flightNumber}>
-                    <div>Voo</div>
-                    <strong>RS{getRandomInt(245416)}</strong>
-                </div>
-                <div className={styles.flightDate}>
-                    <div>Data</div>
-                    <strong>{date}</strong>
-                </div>
-            </div>
-            <div className={styles.footerFlight}>
-                <div className={styles.departure}>
-                    <div className={styles.city}>{departure.aeroporto}</div>
-                    <strong className={styles.abreviationAirport}>
-                        {departure.codigoIata}
-                    </strong>
-                    <div className={styles.hour}>{time}</div>
-                </div>
-                <div className={styles.planeImage}>
-                    <img src={plane} alt="a little plane image" />
-                </div>
-                <div className={styles.arrival}>
-                    <div className={styles.city}>{destination.aeroporto}</div>
-                    <strong className={styles.abreviationAirport}>
-                        {destination.codigoIata}
-                    </strong>
-                    <div className={styles.hour}>04:48</div>
-                </div>
-            </div>
+  return (
+    <div className={styles.flight}>
+      <div className={styles.containerFlight}>
+        <div className={styles.headerFlight}>
+          <div className={styles.flightNumber}>
+            <div>Voo</div>
+            <strong>RS{getRandomInt(245416)}</strong>
+          </div>
+          <div className={styles.flightDate}>
+            <div>Data</div>
+            <strong>{date}</strong>
+          </div>
         </div>
-    );
+        <div className={styles.footerFlight}>
+          <div className={styles.departure}>
+            <div className={styles.cityDeparture}>{departure.aeroporto}</div>
+            <strong className={styles.abreviationAirport}>
+              {departure.codigoIata}
+            </strong>
+            <div className={styles.hour}>{time}</div>
+          </div>
+          <div className={styles.planeImage}>
+            <img src={plane} alt="a little plane image" />
+          </div>
+          <div className={styles.arrival}>
+            <div className={styles.cityDestination}>
+              {destination.aeroporto}
+            </div>
+            <strong className={styles.abreviationAirport}>
+              {destination.codigoIata}
+            </strong>
+            <div className={styles.hour}>04:48</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Flight;
